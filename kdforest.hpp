2ff7687053c6872b627da13233d9f4605d811abd
Steven Braeger <steve@soapforge.com>
2018-02-27 14:48:17 -0500

    Update kdforest.hpp

--- kdforest.hpp
+++ kdforest.hpp
@@ -8,25 +8,25 @@ template<class FloatType>
 class kdforest: public abstract_range_search<FloatType>
 {
 public:
-	const size_t num_forests;
+	const size_t num_trees;
 private:
 	std::vector<std::vector<size_t>> forest_indices;
 	std::vector<std::vector<size_t>> forest_dimension_indices;
 	std::vector<size_t> forest_sizes;
 	size_t max_size;
 private:
-	static size_t compute_num_forests(size_t n,size_t feature_size,size_t tnum_forests)
+	static size_t compute_num_trees(size_t n,size_t feature_size,size_t tnum_trees)
 	{
 		size_t recommended_max_size;
 		for(recommended_max_size=0;(size_t(1) << recommended_max_size) <= n;recommended_max_size++);
 		recommended_max_size-=1;
 
-		if(tnum_forests==0)
+		if(tnum_trees==0)
 		{
 			return feature_size/recommended_max_size + ((feature_size % recommended_max_size) ? 1 : 0);
 		}
 
-		size_t requested_max_size=feature_size / tnum_forests;
+		size_t requested_max_size=feature_size / tnum_trees;
 		
 		if(requested_max_size > recommended_max_size)
 		{
@@ -35,24 +35,24 @@ private:
 		return feature_size/requested_max_size+((feature_size % requested_max_size) ? 1 : 0);
 	}
 public:
-	kdforest(const std::vector<FloatType>& tdata,size_t tfeature_size,size_t tnum_forests=0):
+	kdforest(const std::vector<FloatType>& tdata,size_t tfeature_size,size_t tnum_trees=0):
 		abstract_range_search(tdata,tfeature_size),
-		num_forests(compute_num_forests(num_features,tfeature_size,tnum_forests)),
-		forest_indices(num_forests),
-		forest_dimension_indices(num_forests),
-		forest_sizes(num_forests)
+		num_trees(compute_num_trees(num_features,tfeature_size,tnum_trees)),
+		forest_indices(num_trees),
+		forest_dimension_indices(num_trees),
+		forest_sizes(num_trees)
 	{
 		std::vector<size_t> sindices(num_features);
 		std::iota(sindices.begin(),sindices.end(),0);
 
-		size_t mainsize=feature_size / num_forests;
-		size_t lastsize=feature_size % num_forests;
+		size_t mainsize=feature_size / num_trees;
+		size_t lastsize=feature_size % num_trees;
 		std::fill(forest_sizes.begin(),forest_sizes.end(),mainsize);
 		forest_sizes.back()=lastsize;
 
 		max_size=1;
 		
-		for(size_t fi=0;fi<num_forests;fi++)
+		for(size_t fi=0;fi<num_trees;fi++)
 		{
 			forest_indices[fi]=sindices;
 			kdsort(data+fi*mainsize,forest_sizes[fi],
@@ -63,8 +63,8 @@ public:
 	}
 	
 	template<class FeatureFunc>
-	kdforest(FeatureFunc f,size_t tnum_features,size_t tfeature_size,size_t tnum_forests=0):
-		kdforest(build_features_array(f,tnum_features,tfeature_size),tfeature_size,tnum_forests)
+	kdforest(FeatureFunc f,size_t tnum_features,size_t tfeature_size,size_t tnum_trees=0):
+		kdforest(build_features_array(f,tnum_features,tfeature_size),tfeature_size,tnum_trees)
 	{}
 
 	std::vector<size_t> range_query(const FloatType* lower,const FloatType* upper,std::vector<bool> mask=std::vector<bool>())
@@ -80,7 +80,7 @@ public:
 		std::unordered_set<size_t> candidate(16);
 
 		
-		for(size_t fi=0;fi<num_forests;fi++)
+		for(size_t fi=0;fi<num_trees;fi++)
 		{
 			outrange.clear();
 			size_t countdown=~size_t(0);
